a14a43e4e837f1373a20d5c096647a487e51de77
txy <xingye_tian@tju.edu.cn>
2018-03-07 17:16:36 +0800

    [archi] add a new net

--- assignment2/cs231n/architectures.py
+++ assignment2/cs231n/architectures.py
@@ -78,3 +78,60 @@ def my_model_2(X,y,is_training):
     a11 = tf.layers.dense(a10, 10)
     
     return a11
+
+# [conv-relu-conv-relu-pool]x3 -> [affine]x2 -> [softmax]
+def my_model_3(X,y,is_training):
+    
+    # conv-relu-pool 1
+    Wconv1 = tf.get_variable("Wconv1", shape=[3, 3, 3, 32])
+    bconv1 = tf.get_variable("bconv1", shape=[32])
+    # conv-relu-pool 2
+    Wconv2 = tf.get_variable("Wconv2", shape=[3, 3, 32, 64])
+    bconv2 = tf.get_variable("bconv2", shape=[64])
+
+    # conv-relu-pool 3
+    Wconv3 = tf.get_variable("Wconv3", shape=[3, 3, 64, 128])
+    bconv3 = tf.get_variable("bconv3", shape=[128])
+    # conv-relu-pool 4
+    Wconv4 = tf.get_variable("Wconv4", shape=[3, 3, 128, 256])
+    bconv4 = tf.get_variable("bconv4", shape=[256])
+
+    # conv-relu-pool 3
+    Wconv5 = tf.get_variable("Wconv5", shape=[3, 3, 256, 512])
+    bconv5 = tf.get_variable("bconv5", shape=[512])
+    # conv-relu-pool 4
+    Wconv6 = tf.get_variable("Wconv6", shape=[3, 3, 512, 512])
+    bconv6 = tf.get_variable("bconv6", shape=[512])
+
+    
+    a1 = tf.nn.conv2d(X, Wconv1, [1,1,1,1], padding="SAME") + bconv1
+    a1 = tf.nn.relu(a1)
+    a1 = tf.nn.conv2d(X, Wconv2, [1,1,1,1], padding="SAME") + bconv2
+    a1 = tf.nn.relu(a1)
+    a2 = tf.nn.max_pool(a1, [1,2,2,1], [1,2,2,1], padding="SAME")
+    
+    a3 = tf.nn.conv2d(a2, Wconv3, [1,1,1,1], padding="SAME") + bconv3
+    a3 = tf.nn.relu(a3)
+    a3 = tf.nn.conv2d(a2, Wconv4, [1,1,1,1], padding="SAME") + bconv4
+    a3 = tf.nn.relu(a3)
+    a4 = tf.nn.max_pool(a3, [1,2,2,1], [1,2,2,1], padding="SAME")
+   
+    a5 = tf.nn.conv2d(a4, Wconv5, [1,1,1,1], padding="SAME") + bconv5
+    a5 = tf.nn.relu(a5)
+    a5 = tf.nn.conv2d(a5, Wconv6, [1,1,1,1], padding="SAME") + bconv6
+    a5 = tf.nn.relu(a5)
+    a6 = tf.nn.max_pool(a5, [1,2,2,1], [1,2,2,1], padding="SAME")
+    
+
+       
+    a6_reshape = tf.reshape(a6, [-1,8192])
+    a7 = tf.layers.dense(a6_reshape, 1000, activation=tf.nn.relu)
+    a8 = tf.layers.dense(a7, 100, activation=tf.nn.relu)
+    a9 = tf.layers.dense(a8, 10)
+    
+    return a9
+
+
+
+
+
