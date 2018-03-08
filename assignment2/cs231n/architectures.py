fe026a758b33d696518b556304cd18062f29e697
txy <xingye_tian@tju.edu.cn>
2018-03-08 14:06:10 +0800

    [dropout] fixed dropout_forward

--- assignment2/cs231n/architectures.py
+++ assignment2/cs231n/architectures.py
@@ -1,11 +1,48 @@
 # Different CNN architectures defined in this file.
 # Input image size is [32, 32, 3]
 
-# [conv-relu-pool]x3 -> [affine]x2 -> [softmax]
 import tensorflow as tf
 
+
+# [conv-relu-pool]x3 -> [affine]x2 -> [softmax]
 def my_model_1(X,y,is_training):
     
+    # conv-relu-pool 1
+    Wconv1 = tf.get_variable("Wconv1", shape=[3, 3, 3, 32])
+    bconv1 = tf.get_variable("bconv1", shape=[32])
+    # conv-relu-pool 2
+    Wconv2 = tf.get_variable("Wconv2", shape=[3, 3, 32, 64])
+    bconv2 = tf.get_variable("bconv2", shape=[64])
+    # conv-relu-pool 3
+    Wconv3 = tf.get_variable("Wconv3", shape=[3, 3, 64, 128])
+    bconv3 = tf.get_variable("bconv3", shape=[128])
+    
+    a1 = tf.nn.conv2d(X, Wconv1, [1,1,1,1], padding="SAME") + bconv1
+    a2 = tf.nn.relu(a1)
+    a3 = tf.nn.max_pool(a2, [1,2,2,1], [1,2,2,1], padding="SAME")
+    
+    a4 = tf.nn.conv2d(a3, Wconv2, [1,1,1,1], padding="SAME") + bconv2
+    a5 = tf.nn.relu(a4)
+    a6 = tf.nn.max_pool(a5, [1,2,2,1], [1,2,2,1], padding="SAME")
+    
+    a7 = tf.nn.conv2d(a6, Wconv3, [1,1,1,1], padding="SAME") + bconv3
+    a8 = tf.nn.relu(a7)
+    a9 = tf.nn.max_pool(a8, [1,2,2,1], [1,2,2,1], padding="SAME")
+    
+    
+    
+    a9_reshape = tf.reshape(a9, [-1,2048])
+    a10 = tf.layers.dense(a9_reshape, 1024, activation=tf.nn.relu)
+    a11 = tf.layers.dense(a10, 100, activation=tf.nn.relu)
+    a12 = tf.layers.dense(a11, 10)
+    
+    return a12
+
+def my_model_1_dropout(X,y,is_training):
+    '''
+    Because my_model_1 overfit the training dataset, add dropout layer to the model.    
+    '''
+
     # conv-relu-pool 1
     Wconv1 = tf.get_variable("Wconv1", shape=[3, 3, 3, 32])
     bconv1 = tf.get_variable("bconv1", shape=[32])
@@ -112,7 +149,7 @@ def my_model_3(X,y,is_training):
     
     a4 = tf.nn.conv2d(a3, Wconv3, [1,1,1,1], padding="SAME") + bconv3
     a4 = tf.nn.relu(a4)
-    a6 = tf.nn.conv2d(a5, Wconv4, [1,1,1,1], padding="SAME") + bconv4
+    a6 = tf.nn.conv2d(a4, Wconv4, [1,1,1,1], padding="SAME") + bconv4
     a6 = tf.nn.relu(a6)
     a7 = tf.nn.max_pool(a6, [1,2,2,1], [1,2,2,1], padding="SAME")
    
@@ -121,9 +158,7 @@ def my_model_3(X,y,is_training):
     a9 = tf.nn.conv2d(a8, Wconv6, [1,1,1,1], padding="SAME") + bconv6
     a9 = tf.nn.relu(a9)
     a10 = tf.nn.max_pool(a9, [1,2,2,1], [1,2,2,1], padding="SAME")
-    
-
-       
+          
     a10_reshape = tf.reshape(a10, [-1,8192])
     a11 = tf.layers.dense(a10_reshape, 1000, activation=tf.nn.relu)
     a12 = tf.layers.dense(a11, 100, activation=tf.nn.relu)
