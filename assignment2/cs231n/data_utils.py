74b8a608e07e31fdb47f61fe6adc108a40e2ebc8
txy <xingye_tian@tju.edu.cn>
2018-03-15 11:34:35 +0800

    [pytorch] start pytorch learning

--- assignment2/cs231n/data_utils.py
+++ assignment2/cs231n/data_utils.py
@@ -49,7 +49,7 @@ def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=1000,
     condensed to a single function.
     """
     # Load the raw CIFAR-10 data
-    cifar10_dir = '/home/txy/assignment2/cs231n/datasets'
+    cifar10_dir = '/notebooks/assignment2/cs231n/datasets'
     X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
 
     # Subsample the data
